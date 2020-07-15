710529324533b5e2bd8fea5e64b789c2763e8a95
David Klein <dave@smokinheads.de>
2020-07-15 23:08:52 +0200

    --remote update all submodules

--- src/Main.hx
+++ src/Main.hx
@@ -8,6 +8,8 @@ class Main {
 
 				final state = new AppState();
 				final root = new Component();
+				root.percentWidth = 100;
+				root.percentHeight = 100;
 
 				Renderer.mount(root, coconut.Ui.hxx('<App state=${state}/>'));
 				Screen.instance.addComponent(root);
@@ -15,7 +17,7 @@ class Main {
 				kha.System.notifyOnFrames(function( fbs ) {
 					final fb = fbs[0];
 					final g2 = fb.g2;
-					g2.begin(true, 0x800020);
+					g2.begin(true, 0xff008080);
 						Screen.instance.renderTo(g2);
 					g2.end();
 				});
